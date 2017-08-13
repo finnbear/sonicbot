7bd6dc929c024728c37406bd82082b08b37c237c
finnbear <finnbearone@gmail.com>
2017-08-12 17:58:46 -0700

    All pinmodes.

--- SonicBot/SonicBot.ino
+++ SonicBot/SonicBot.ino
@@ -1,6 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+int leftMotorDirectionPin = 8;
+int leftMotorSpeedPin = 9;
+int rightMotorDirectionPin = 2;
+int rightMotorSpeedPin = 3;
 
+int sonicServoPin = 5;
+int sonicTriggerPin = 4;
+int sonicEchoPin = 7;
+
+void setup() {
+  pinMode(leftMotorDirectionPin, OUTPUT);
+  pinMode(leftMotorSpeedPin, OUTPUT);
+  pinMode(rightMotorDirectionPin, OUTPUT);
+  pinMode(rightMotorSpeedPin, OUTPUT);
+
+  pinMode(sonicServoPin, OUTPUT);
+  pinMode(sonicTriggerPin, OUTPUT);
+  pinMode(sonicEchoPin, INPUT);
 }
 
 void loop() {
